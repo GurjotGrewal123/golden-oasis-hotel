087f6550ef49f4bad90e1893b642c9d87e3b55bc
Ryan Frost-Garant <rfros048@uottawa.ca>
2024-03-24 18:02:41 -0400

    2c queries

--- server/index.js
+++ server/index.js
@@ -11,6 +11,74 @@ app.use(express.json());
 
 // API Calls //
 
+//Get hotel information
+app.get("/hotel_info", async(req, res) => {
+  try{
+    const {chain_name,street_name,street_number,city,province_state} = req.body;
+
+    const hotels = await pool.query(
+      "SELECT chain_name,street_name,street_number,city,province_state FROM hotel",
+      [chain_name,street_name,street_number,city,province_state]
+    );
+
+    res.json(hotels.rows);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Get customer information
+app.get("/customer_info", async(req, res) => {
+  try{
+    const {full_name, customer_email, street_number,street_name, city, province_state} = req.body;
+
+    const customers = await pool.query(
+      "SELECT full_name, customer_email, street_number,street_name, city, province_state FROM customers",
+      [full_name, customer_email, street_number,street_name, city, province_state]
+    );
+
+    res.json(customers.rows);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Avg Rating of each hotel chain
+app.get("/hotel_avg", async(req, res) => {
+  try{
+    const {chain_name,avg} = req.body;
+
+    const chain_avgs = await pool.query(
+      "SELECT chain_name, avg(category) AS avg_rating FROM hotels GROUP BY chain_name;",
+      [chain_name,avg]
+    );
+
+    res.json(chain_avgs.rows);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//Select 5 Star Rated hotels, where the chains do not have 1 or 2 rated hotels
+app.get("/five_star", async(req, res) => {
+  try{
+    const {chain_name,street_name, street_number, city, province_state} = req.body;
+
+    const fives = await pool.query(
+      "select chain_name,street_name, street_number, city, province_state from hotels where category = 5 and chain_name not in (select chain_name from hotels where category = 2) and chain_name not in (select chain_name from hotels where category = 1)",
+      [chain_name,street_name, street_number, city, province_state]
+    );
+
+    res.json(fives.rows);
+  }catch(err){
+    console.error(err.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 // customer creates a booking
 app.post("/bookings", async (req, res) => {
   try {
